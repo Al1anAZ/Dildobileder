b80aea606daf265ecc2edd5c41513fe9e96368ee
Artem <artemzagovora@gmail.com>
2023-09-13 12:52:05 +0300

    Extra services transition

--- js/script.js
+++ js/script.js
@@ -4,17 +4,46 @@ window.addEventListener(`scroll`, () =>{
     (document.documentElement.scrollTop > 0) ? Header.style.backgroundColor = "rgba(35, 31, 32, 0.95)" : Header.style.backgroundColor = "transparent";
 });
 let ExtraServicesCheck = true;
-function ShowOrHideExtraServices(){
-    if(ExtraServicesCheck){
-        document.getElementById(`ExtraServices`).style.display = `flex`;
-        document.getElementById(`Moretext`).style.display = `none`;
-        document.getElementById(`ShowOrHideExtraServButton`).style.transform = `rotate(180deg)`;
-        ExtraServicesCheck = false;
+// function ShowOrHideExtraServices(){
+//     if(ExtraServicesCheck){
+//         document.getElementById(`ExtraServices`).style.transition = `opacity 1s`
+//         document.getElementById(`ExtraServices`).style.opacity  = 1;
+//         document.getElementById(`ExtraServices`).style.height = `auto`;
+//         document.getElementById(`Moretext`).style.display = `none`;
+//         document.getElementById(`ShowOrHideExtraServButton`).style.transform = `rotate(180deg)`;
+//         ExtraServicesCheck = false;
+//     }
+//     else{
+//         document.getElementById(`ExtraServices`).style.transition = `opacity 0s`
+//         document.getElementById(`ExtraServices`).style.opacity  = 0;
+//         document.getElementById(`ExtraServices`).style.height = `0`;
+//         document.getElementById(`Moretext`).style.display = `block`;
+//         document.getElementById(`ShowOrHideExtraServButton`).style.transform = `rotate(0)`;
+//         ExtraServicesCheck = true;
+//     }
+    
+// }
+const ExtraServices = document.getElementById(`ExtraServices`);
+const ShowOrHideExtraServButton = document.getElementById(`ShowOrHideExtraServButton`);
+const Moretext = document.getElementById(`Moretext`);
+ShowOrHideExtraServButton.addEventListener("transitionend", () => {
+    if (ExtraServices.style.height !== "0px") {
+        ExtraServices.style.height = "auto"
     }
-    else{
-        document.getElementById(`ExtraServices`).style.display = `none`;
-        document.getElementById(`Moretext`).style.display = `block`;
-        document.getElementById(`ShowOrHideExtraServButton`).style.transform = `rotate(0)`;
-        ExtraServicesCheck = true;
+});
+ShowOrHideExtraServButton.addEventListener("click", () => {
+    if (ExtraServices.style.height === "0px") {
+        ExtraServices.style.height = `${ ExtraServices.scrollHeight }px`;
+        ExtraServices.style.opacity =  1;
+        ShowOrHideExtraServButton.style.transform = `rotate(180deg)`;
+        Moretext.style.display = `none`;
+    } 
+    else {
+        ExtraServices.style.height = `${ ExtraServices.scrollHeight }px`;
+        window.getComputedStyle(ExtraServices, null).getPropertyValue("height");
+        ExtraServices.style.height = "0";
+        ExtraServices.style.opacity =  0;
+        ShowOrHideExtraServButton.style.transform = `rotate(0)`;
+        Moretext.style.display = `block`;
     }
-}
+});
