fe5ea59c57177123ba745a59fb10df559bbc390d
Artem <artemzagovora@gmail.com>
2023-09-13 17:54:24 +0300

    Add burger

--- js/script.js
+++ js/script.js
@@ -1,31 +1,9 @@
 "use strict";
-window.addEventListener(`scroll`, () =>{
-    let Header = document.querySelector('.HeaderContainer');
-    (document.documentElement.scrollTop > 0) ? Header.style.backgroundColor = "rgba(35, 31, 32, 0.95)" : Header.style.backgroundColor = "transparent";
-});
-let ExtraServicesCheck = true;
-// function ShowOrHideExtraServices(){
-//     if(ExtraServicesCheck){
-//         document.getElementById(`ExtraServices`).style.transition = `opacity 1s`
-//         document.getElementById(`ExtraServices`).style.opacity  = 1;
-//         document.getElementById(`ExtraServices`).style.height = `auto`;
-//         document.getElementById(`Moretext`).style.display = `none`;
-//         document.getElementById(`ShowOrHideExtraServButton`).style.transform = `rotate(180deg)`;
-//         ExtraServicesCheck = false;
-//     }
-//     else{
-//         document.getElementById(`ExtraServices`).style.transition = `opacity 0s`
-//         document.getElementById(`ExtraServices`).style.opacity  = 0;
-//         document.getElementById(`ExtraServices`).style.height = `0`;
-//         document.getElementById(`Moretext`).style.display = `block`;
-//         document.getElementById(`ShowOrHideExtraServButton`).style.transform = `rotate(0)`;
-//         ExtraServicesCheck = true;
-//     }
-    
-// }
 const ExtraServices = document.getElementById(`ExtraServices`);
 const ShowOrHideExtraServButton = document.getElementById(`ShowOrHideExtraServButton`);
 const Moretext = document.getElementById(`Moretext`);
+const BurgerTogle = document.getElementById(`menu__toggle`);
+const Header = document.querySelector('.HeaderContainer');
 ShowOrHideExtraServButton.addEventListener("transitionend", () => {
     if (ExtraServices.style.height !== "0px") {
         ExtraServices.style.height = "auto"
@@ -46,4 +24,11 @@ ShowOrHideExtraServButton.addEventListener("click", () => {
         ShowOrHideExtraServButton.style.transform = `rotate(0)`;
         Moretext.style.display = `block`;
     }
+});
+BurgerTogle.addEventListener(`click`, ()=>{
+   BurgerTogle.checked ? Header.style.backgroundColor = "rgba(35, 31, 32, 0.95)" : false;
+      
+})
+window.addEventListener(`scroll`, () =>{
+    (document.documentElement.scrollTop > 0) ? Header.style.backgroundColor = "rgba(35, 31, 32, 0.95)" : Header.style.backgroundColor = "transparent";
 });
