3a84df9b7d0047b9d6efece103f2fc7927cc302a
Artem <artemzagovora@gmail.com>
2023-09-29 03:04:48 +0300

    fixed bug with extrabutton

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 "use strict";
-const ExtraServices = document.getElementById(`ExtraServices`);
+let ExtraServices = document.getElementById(`ExtraServices`);
 const ShowOrHideExtraServButton = document.getElementById(`ShowOrHideExtraServButton`);
 const Moretext = document.getElementById(`Moretext`);
 const BurgerTogle = document.getElementById(`menu__toggle`);
-const Header = document.querySelector('.HeaderContainer');
+const Header = document.querySelector('nav');
 ShowOrHideExtraServButton.addEventListener("transitionend", () => {
     if (ExtraServices.style.height !== "0px") {
         ExtraServices.style.height = "auto"
@@ -14,15 +14,15 @@ ShowOrHideExtraServButton.addEventListener("click", () => {
         ExtraServices.style.height = `${ ExtraServices.scrollHeight }px`;
         ExtraServices.style.opacity =  1;
         ShowOrHideExtraServButton.style.transform = `rotate(180deg)`;
-        Moretext.style.display = `none`;
+        Moretext.hidden = true;
     } 
     else {
         ExtraServices.style.height = `${ ExtraServices.scrollHeight }px`;
         window.getComputedStyle(ExtraServices, null).getPropertyValue("height");
-        ExtraServices.style.height = "0";
+        ExtraServices.style.height = 0;
         ExtraServices.style.opacity =  0;
         ShowOrHideExtraServButton.style.transform = `rotate(0)`;
-        Moretext.style.display = `block`;
+        Moretext.hidden = false;
     }
 });
 BurgerTogle.addEventListener(`click`, ()=>{
